1753a8146edec11f0e1953bb8ded32ef813ff40e
Doga Tolgay <56132931+dtolgay@users.noreply.github.com>
2025-11-03 13:54:43 -0500

    Done until case

--- 02_activities/assignments/DC_Cohort/assignment1.sql
+++ 02_activities/assignments/DC_Cohort/assignment1.sql
@@ -4,17 +4,21 @@
 
 --SELECT
 /* 1. Write a query that returns everything in the customer table. */
-
+SELECT * 
+	FROM customer;
 
 
 /* 2. Write a query that displays all of the columns and 10 rows from the cus- tomer table, 
 sorted by customer_last_name, then customer_first_ name. */
-
+SELECT * FROM customer
+	ORDER BY customer_last_name, customer_first_name
+	LIMIT 10;
 
 
 --WHERE
 /* 1. Write a query that returns all customer purchases of product IDs 4 and 9. */
-
+SELECT * FROM customer_purchases
+	WHERE product_id = 4 AND product_id = 9;
 
 
 /*2. Write a query that returns all customer purchases and a new calculated column 'price' (quantity * cost_to_customer_per_qty), 
@@ -23,10 +27,15 @@ filtered by customer IDs between 8 and 10 (inclusive) using either:
 	2.  one condition using BETWEEN
 */
 -- option 1
-
+SELECT *, (quantity * cost_to_customer_per_qty) AS price
+	FROM customer_purchases
+	WHERE customer_id >= 8 AND customer_id <= 10; 
 
 -- option 2
-
+SELECT *, (quantity * cost_to_customer_per_qty) AS price
+	FROM customer_purchases
+	WHERE customer_id BETWEEN 8 AND 16; 
+	
 
 
 --CASE
